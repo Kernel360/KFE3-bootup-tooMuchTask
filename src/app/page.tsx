9b8650d4f36e332308789717bfb59db965362b74
젤리 <stay@jelliui-MacBookAir.local>
2025-06-06 23:46:08 +0900

    feat: InputCheckBox 컴포넌트 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+'use client';
+
+import { InputCheckBox } from '@/components/Input/InputCheckBox';
+
 export default function Home() {
-  return <></>;
+  function HandleChangeCheck() {
+    console.log('check');
+  }
+  return (
+    <div>
+      <InputCheckBox label='테스트' id='123' onChange={HandleChangeCheck} hiddenlabel />
+    </div>
+  );
 }
