466a2a0d41de08619f676616fa45b5b12bf5a60d
젤리 <stay@jelliui-MacBookAir.local>
2025-06-06 21:46:54 +0900

    fix:Header 스타일링 및 route 위치별 레이아웃 변경

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,24 +1,88 @@
 'use client';
 
-import { useParams, usePathname, useSearchParams } from 'next/navigation';
-import { Wrapper } from './style';
-import { Accordion } from '../Accordion';
+import { useEffect, useState } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
 import Link from 'next/link';
+import { IconButton, RightArea, Wrapper, Title } from './style';
+import { Accordion } from '../Accordion';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+interface location {
+  name: string;
+}
 
 export const Header = () => {
+  const [headerType, setHeaderType] = useState<string | null>();
+  const [name, setName] = useState<string>('상대방 닉네임');
+  const [locations, setLocations] = useState<location[]>([]);
   const pathname = usePathname();
-  const path = pathname.split('/');
+  const router = useRouter();
+
+  const handleBack = () => router.back();
+  const getHeaderType = (pathname: string): string | null => {
+    if (pathname === '/') return 'home';
+    if (pathname.startsWith('/product')) return 'detail';
+    if (pathname.startsWith('/chat')) return 'chat';
+    if (pathname.startsWith('/profile')) return 'profile';
+    return null;
+  };
+
+  useEffect(() => {
+    setHeaderType(getHeaderType(pathname));
+  }, [pathname]);
 
   return (
     <>
-      <Wrapper>
-        <Accordion buttonTitle='버튼명'>
-          <li>메뉴 1</li>
-          <li>메뉴 2</li>
-          <li>
-            <Link href={'/'}>거래지역 수정</Link>
-          </li>
-        </Accordion>
+      <Wrapper headerType={headerType!}>
+        {headerType !== 'home' && (
+          <IconButton aria-label='뒤로 가기' onClick={handleBack}>
+            <ArrowBackIosNewIcon />
+          </IconButton>
+        )}
+
+        {headerType === 'home' && (
+          <>
+            <Accordion buttonTitle='버튼명'>
+              {locations?.map(({ name }, idx) => {
+                return <li key={idx}>{name}</li>;
+              })}
+              <li>
+                <Link href={'/'}>거래지역 수정</Link>
+              </li>
+            </Accordion>
+
+            {/* onClickEvent추가 */}
+            <IconButton aria-label='알람 보기'>
+              <NotificationsIcon />
+            </IconButton>
+          </>
+        )}
+
+        {headerType === 'detail' && (
+          // onClickEvent추가
+          <IconButton aria-label='더보기'>
+            <MoreVertIcon />
+          </IconButton>
+        )}
+
+        {headerType === 'chat' && (
+          <>
+            <Title>{name}</Title>
+            {/* onClickEvent추가 */}
+            <IconButton aria-label='더보기'>
+              <MoreVertIcon />
+            </IconButton>
+          </>
+        )}
+
+        {headerType === 'profile' && (
+          <>
+            <Title>설정 페이지명</Title>
+            <RightArea />
+          </>
+        )}
       </Wrapper>
     </>
   );
