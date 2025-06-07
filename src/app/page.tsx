f3862de81cd3d4b0251b2c5bca3d2350ca48ea56
젤리 <stay@jelliui-MacBookAir.local>
2025-06-07 11:17:59 +0900

    move: Header, Accordion 폴더 위치 수정 및 app/componets 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,5 @@
 import { InputCheckBox } from '@/components/Input/InputCheckBox';
 
 export default function Home() {
-  function HandleChangeCheck() {
-    console.log('check');
-  }
-  return (
-    <div>
-      <InputCheckBox label='테스트' id='123' onChange={HandleChangeCheck} hiddenlabel />
-    </div>
-  );
+  return <div></div>;
 }
