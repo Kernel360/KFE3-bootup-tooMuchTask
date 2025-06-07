d18e3ffe56d5d941c209bb11eaf84585391993e7
젤리 <stay@jelliui-MacBookAir.local>
2025-06-07 12:16:37 +0900

    fix: app/page.tsx 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-'use client';
-
-import { InputToggle } from '@/components/Input/InputToggle';
-
 export default function Home() {
-  const onChange = () => {
-    console.log('toggle 테스트');
-  };
-
-  return (
-    <div>
-      <InputToggle id='test' label='테스트' onChange={onChange} />
-    </div>
-  );
+  return <div></div>;
 }
