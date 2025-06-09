2c12f10d2e0f3607bc86ad9c2073173c7accdc37
Seoyoung Park <seokachuu@gmail.com>
2025-06-09 22:32:29 +0900

    Setting : 모노레포 세팅 및 보일러플레이트 세팅

---
+++ packages/ui/src/components/common/Accordion/index.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import Link from 'next/link';
+import { ReactNode, useRef, useState } from 'react';
+
+import { AccordionButton, AccordionListWrapper, Wrapper } from './style';
+
+// Accordion 컴포넌트: 접고 펼칠 수 있는 UI를 구현
+export const Accordion = ({
+  children,
+  buttonTitle,
+}: {
+  children: ReactNode;
+  buttonTitle: string;
+}) => {
+  // 펼침 여부 상태 관리 (true: 열림, false: 닫힘)
+  const [active, setActive] = useState<boolean>(false);
+  // 버튼 요소 참조 (향후 포커싱이나 DOM 조작 시 사용 가능)
+  const ref = useRef<HTMLButtonElement>(null);
+
+  // 버튼 클릭 시 상태를 반전시켜 Accordion을 열거나 닫음
+  const handleToggle = () => {
+    setActive((prev) => !prev);
+  };
+
+  return (
+    <Wrapper active={active}>
+      <AccordionButton ref={ref} onClick={handleToggle}>
+        {buttonTitle}
+        <i>
+          <KeyboardArrowDownIcon />
+        </i>
+      </AccordionButton>
+      <AccordionListWrapper>
+        {children}
+        <li>
+          <Link href={'/'}>거래지역관리</Link>
+        </li>
+      </AccordionListWrapper>
+    </Wrapper>
+  );
+};
---
+++ packages/ui/src/components/common/NavBar/styles.ts
@@ -0,0 +1,43 @@
+import styled, { css } from 'styled-components';
+
+const NavBar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 96px;
+  padding: 16px 24px 24px;
+`;
+
+const NavItemWrapper = styled.div.withConfig({
+  shouldForwardProp: (prop) => prop !== '$active',
+})<{ $active?: boolean }>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 6px;
+  width: 56px;
+  text-align: center;
+  font-size: 12px;
+  color: var(--color-neutral-400);
+  text-decoration: none;
+
+  svg {
+    fill: var(--color-neutral-400);
+    width: 20px;
+    height: 20px;
+  }
+
+  ${({ $active }) =>
+    $active &&
+    css`
+      color: var(--color-teal-500);
+
+      svg {
+        fill: var(--color-teal-500);
+      }
+    `}
+`;
+
+const NavIcon = styled.div``;
+
+export { NavBar, NavIcon, NavItemWrapper };
